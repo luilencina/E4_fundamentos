c9bcf197822120cc5ea3067abf2f10233d67c03a
luilencina <luizalencina@hotmail.com>
2020-06-24 16:31:03 -0300

    bugs

--- src/AppBolinhasDeGude.java
+++ src/AppBolinhasDeGude.java
@@ -10,6 +10,7 @@ public class AppBolinhasDeGude {
         String nome = in.nextLine();
         System.out.println("Seja Bem vindo(a) " + nome + "!");
 
+        //para abrir o menu
         System.out.println("Digite 1 para abrir o menu!");
         int menu = in.nextInt();
         while(menu != 1){
@@ -20,28 +21,42 @@ public class AppBolinhasDeGude {
 
         // menu
             do {
-            System.out.println("Selecione o que deseja fazer: ");
+                // espaço so para ficar mais boniitinho <3
+                System.out.println(" . ");
+            System.out.println(" . ");
+            System.out.println(" . ");
+                // espaço so para ficar mais boniitinho <3
+
+                System.out.println("Selecione o que deseja fazer: ");
             System.out.println("1 - Realizar uma compra");
-            System.out.println("2 - Quantidade em estoque");
+            System.out.println("2 - Ver quantidade em estoque");
+            System.out.println("3 - Adicionar bolinhas ao estoque");
             System.out.println("0 - Sair! ");
             menu = in.nextInt();
             //opções
             switch (menu) {
+                case 0:
+                    System.out.println("Volte sempre, Até mais " + nome + "!");
+                    break;
                 case 1:
                     System.out.println("Insira a quantidade de bolinhas que deseja adquirir: ");
                     int quantComprar = in.nextInt();
                     int quantEstoque = bolinhasDeGude.QuantEstoque(quantComprar);
-                    String tam = bolinhasDeGude.tamanhoBolinhas();
-
                     System.out.println("Obrigada pela compra!");
+                    break;
                 case 2:
                     System.out.println("Quantidade em estoque:");
-                case 0:
-                    System.out.println("Volte sempre, Até mais " + nome + "!");
+                    break;
+                case 3:
+                    System.out.println("Insira a quantidade de bolinhas que deseja adicionar ao estoque: ");
+                    int quantAdicionar = in.nextInt();
+                    int quanEstoque = bolinhasDeGude.QuantEstoque(quantAdicionar);
+                    break;
                 default:
                     System.out.println("Opção inválida, Tente novamente seguindo as opções dadas acima.");
+                    break;
                      }
-                } while( menu != 0);
+                } while(menu != 0);
 
     } // fecha main
 
--- src/BolinhasDeGude.java
+++ src/BolinhasDeGude.java
@@ -2,38 +2,41 @@ import java.util.Scanner;
 
 public class BolinhasDeGude {
 
-    public String tamanhoBolinhas(){
+    public int tamanhoBolinhas() {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Escolha o tamanho das bolinhas que deseja comprar:");
+        System.out.println("Escolha o tamanho da(s) bolinha(s): ");
         System.out.println("1 - Pequeno");
         System.out.println("2 - Médio");
         System.out.println("3 - Grande");
         int tam = in.nextInt();
 
         // validação tamanho
-        while( tam < 1 || tam > 3){
+        while (tam < 1 || tam > 3) {
             System.out.println("Opção inválida, Tente novamente seguindo as opções dadas acima.");
             tam = in.nextInt();
         }
 
-        switch(tam){
-            case 1: return "Pequeno";
-            case 2: return "Médio";
-            case 3: return "Grande";
-            default: return "Pequeno";
-        }
+        // switch(tam){
+        //   case 1: return "Pequeno";
+        //  case 2: return "Médio";
+        // case 3: return "Grande";
+        // default: return "Pequeno";
+        //    }
+
+        return tam;
     }
 
-    public int QuantEstoque(int quantComprado){
+    public int QuantEstoque(int quantComprado) {
         Scanner in = new Scanner(System.in);
-        AppBolinhasDeGude appBolinhasDeGude = new AppBolinhasDeGude();
+        // AppBolinhasDeGude appBolinhasDeGude = new AppBolinhasDeGude();
 
         //variaveis
         int quantTotal = 600;
+        int tam = tamanhoBolinhas();
 
         //validacao
-        while(quantComprado > quantTotal){
+        while (quantComprado > quantTotal) {
             System.out.println("Valor indisponivél para compra. Valor de bolinhas disponivéis: ");
             System.out.println(quantTotal);
             quantComprado = in.nextInt();
@@ -43,9 +46,62 @@ public class BolinhasDeGude {
 
         System.out.println("Nova Quantidade em estoque: " + quantEstoque);
         System.out.println("Quantidade Comprada: " + quantComprado);
-        System.out.println("Quantidade de caixas cecessárias para entrega: " + 0);
+        System.out.println("Quantidade de caixas cecessárias para entrega: " + QuantCaixas(tam, quantComprado));
 
         return quantEstoque;
     }
 
+
+    public int QuantCaixas(int tam, int quantComprado) {
+        Scanner in = new Scanner(System.in);
+
+        //variaveis
+        int quantCaixas = 0;
+
+        switch (tam) {
+            case 1:
+                quantCaixas = quantComprado % 150;
+                if (quantCaixas == 0) {
+                    quantCaixas = quantComprado / 150;
+                } else {
+                    quantCaixas = (quantComprado / 150) + 1;
+                }
+                System.out.println("Você selecionou o tamanho PEQUENO!");
+                break;
+
+            case 2:
+                quantCaixas = quantComprado % 100;
+                if (quantCaixas == 0) {
+                    quantCaixas = quantComprado / 100;
+                } else {
+                    quantCaixas = (quantComprado / 100) + 1;
+                }
+                System.out.println("Você selecionou o tamanho MÉDIO!");
+                break;
+
+            case 3:
+                quantCaixas = quantComprado % 50;
+                if (quantCaixas == 0) {
+                    quantCaixas = quantComprado / 50;
+                } else {
+                    quantCaixas = (quantComprado / 50) + 1;
+                }
+                System.out.println("Você selecionou o tamanho GRANDE!");
+                break;
+
+            //caso nao tenha tamanho assumir que ela é pequena :)
+            default:
+                quantCaixas = quantComprado % 150;
+                if (quantCaixas == 0) {
+                    quantCaixas = quantComprado / 150;
+                } else {
+                    quantCaixas = (quantComprado / 150) + 1;
+                }
+                System.out.println("Você selecionou o tamanho PEQUENO!");
+                break;
+
+        }
+
+        return quantCaixas;
+    }
 }
